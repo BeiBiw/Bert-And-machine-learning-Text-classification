d758d26df9bd0af14ad23dc0dcdf9f2ef3303ea5
BeiBiw <109725158+BeiBiw@users.noreply.github.com>
2025-01-15 18:09:36 +0800

    Update model.py

--- Bert/model.py
+++ Bert/model.py
@@ -12,9 +12,8 @@ class BertClassfication(nn.Module):
 
     def forward(self, input_ids,attention_mask):
         hiden_outputs = self.model(input_ids, attention_mask=attention_mask)
-        # bert的输出结果有四个维度：
-        # last_hidden_state：shape是(batch_size, sequence_length, hidden_size)，hidden_size=768,它是模型最后一层输出的隐藏状态。
-        # 此时shape是(batch_size, sequence_length, hidden_size)，[:,0,:]的意思是取出第一个也就是cls对应的结果
-        outputs = hiden_outputs[0][:, 0, :]  # [0]表示输出结果部分，[:,0,:]表示[CLS]对应的结果
+        # last_hidden_state：shape是(batch_size, sequence_length, hidden_size)，hidden_size=768
+        # shape(batch_size, sequence_length, hidden_size)
+        outputs = hiden_outputs[0][:, 0, :]
         output = self.fc(outputs)
         return output
